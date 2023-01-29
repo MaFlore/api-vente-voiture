795d0df84aa8414deafdf45554dcde82b413b1c8
AHADJITSE Yawo Florent Mathis <yawo.ahadjitse@ipnetinstitute.com>
2023-01-29 00:19:20 +0000

    Commit 7 = Revue du contrôleur Marque

--- src/main/java/com/projet/classe/vente/controller/MarqueController.java
+++ src/main/java/com/projet/classe/vente/controller/MarqueController.java
@@ -1,11 +1,14 @@
 package com.projet.classe.vente.controller;
 
+import com.projet.classe.vente.model.Historique;
 import com.projet.classe.vente.model.Marque;
+import com.projet.classe.vente.service.HistoriqueService;
 import com.projet.classe.vente.service.MarqueService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -16,6 +19,11 @@ public class MarqueController {
     @Autowired
     public MarqueService marqueService;
 
+    @Autowired
+    public HistoriqueService historiqueService;
+
+    public Historique historique;
+
     @RequestMapping(value = "/marques", method = RequestMethod.GET)
     public List<Marque> getAllMarques() {
 
@@ -23,6 +31,10 @@ public class MarqueController {
 
         try {
             marques = this.marqueService.getAll();
+
+            historique.setDescription("Consultation de la liste des marques");
+            historique.setDateHistorique(new Date());
+            this.historiqueService.save(historique);
         } catch (Exception e) {
             // TODO: handle exception
             System.out.println("Erreur " + e.getMessage());
@@ -39,6 +51,10 @@ public class MarqueController {
 
         try {
             marque = this.marqueService.findById(id);
+
+            historique.setDescription("Affichage de la marque " + marque.getCode());
+            historique.setDateHistorique(new Date());
+            this.historiqueService.save(historique);
         } catch (Exception e) {
             System.out.println("Erreur " + e.getMessage());
         }
@@ -51,6 +67,10 @@ public class MarqueController {
 
         try {
             marque = this.marqueService.save(marque);
+
+            historique.setDescription("Ajout de la marque " + marque.getCode());
+            historique.setDateHistorique(new Date());
+            this.historiqueService.save(historique);
         } catch (Exception e) {
             System.out.println("Erreur " + e.getMessage());
         }
@@ -58,11 +78,15 @@ public class MarqueController {
         return marque;
     }
 
-    @RequestMapping(value = "/marque/modifier/{id}", method = RequestMethod.PUT, headers = "accept=Application/json")
+    @RequestMapping(value = "/marque/modifier", method = RequestMethod.PUT, headers = "accept=Application/json")
     public Marque updateMarque(@RequestBody Marque marque) {
 
         try {
             marque = this.marqueService.update(marque);
+
+            historique.setDescription("Modification de la marque " + marque.getCode());
+            historique.setDateHistorique(new Date());
+            this.historiqueService.save(historique);
         } catch (Exception e) {
             System.out.println("Erreur " + e.getMessage());
         }
@@ -74,5 +98,8 @@ public class MarqueController {
     @RequestMapping(value = "/marque/supprimer/{id}", method = RequestMethod.DELETE, headers = "accept=Application/json")
     public void deleteMarque(@PathVariable Long id) {
         this.marqueService.deleteById(id);
+        historique.setDescription("Suppression de la marque " + id);
+        historique.setDateHistorique(new Date());
+        this.historiqueService.save(historique);
     }
 }
