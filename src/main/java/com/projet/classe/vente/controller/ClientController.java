3df8365a0492fee4ed2967163c52b0ffa999c935
AHADJITSE Yawo Florent Mathis <yawo.ahadjitse@ipnetinstitute.com>
2023-01-29 00:37:52 +0000

    Commit 9 = Revue du contrôleur Client

--- src/main/java/com/projet/classe/vente/controller/ClientController.java
+++ src/main/java/com/projet/classe/vente/controller/ClientController.java
@@ -1,11 +1,14 @@
 package com.projet.classe.vente.controller;
 
 import com.projet.classe.vente.model.Client;
+import com.projet.classe.vente.model.Historique;
 import com.projet.classe.vente.service.ClientService;
+import com.projet.classe.vente.service.HistoriqueService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -16,6 +19,11 @@ public class ClientController {
     @Autowired
     public ClientService clientService;
 
+    @Autowired
+    public HistoriqueService historiqueService;
+
+    public Historique historique;
+
     @RequestMapping(value = "/clients", method = RequestMethod.GET)
     public List<Client> getAllClients() {
 
@@ -23,6 +31,10 @@ public class ClientController {
 
         try {
             clients = this.clientService.getAll();
+
+            historique.setDescription("Consultation de la liste des clients");
+            historique.setDateHistorique(new Date());
+            this.historiqueService.save(historique);
         } catch (Exception e) {
             // TODO: handle exception
             System.out.println("Erreur " + e.getMessage());
@@ -39,6 +51,10 @@ public class ClientController {
 
         try {
             client = this.clientService.findById(id);
+
+            historique.setDescription("Affichage du client " + client.getNom());
+            historique.setDateHistorique(new Date());
+            this.historiqueService.save(historique);
         } catch (Exception e) {
             System.out.println("Erreur " + e.getMessage());
         }
@@ -51,6 +67,10 @@ public class ClientController {
 
         try {
             client = this.clientService.save(client);
+
+            historique.setDescription("Ajout du client " + client.getNom());
+            historique.setDateHistorique(new Date());
+            this.historiqueService.save(historique);
         } catch (Exception e) {
             System.out.println("Erreur " + e.getMessage());
         }
@@ -58,11 +78,15 @@ public class ClientController {
         return client;
     }
 
-    @RequestMapping(value = "/client/modifier/{id}", method = RequestMethod.PUT, headers = "accept=Application/json")
+    @RequestMapping(value = "/client/modifier", method = RequestMethod.PUT, headers = "accept=Application/json")
     public Client updateClient(@RequestBody Client client) {
 
         try {
             client = this.clientService.update(client);
+
+            historique.setDescription("Modification du client " + client.getNom());
+            historique.setDateHistorique(new Date());
+            this.historiqueService.save(historique);
         } catch (Exception e) {
             System.out.println("Erreur " + e.getMessage());
         }
@@ -74,5 +98,8 @@ public class ClientController {
     @RequestMapping(value = "/client/supprimer/{id}", method = RequestMethod.DELETE, headers = "accept=Application/json")
     public void deleteClient(@PathVariable Long id) {
         this.clientService.deleteById(id);
+        historique.setDescription("Suppression du client " + id);
+        historique.setDateHistorique(new Date());
+        this.historiqueService.save(historique);
     }
 }
